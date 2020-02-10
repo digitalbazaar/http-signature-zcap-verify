89a648d042036c94cc329f0e4a9fd48ee27a5ae9
Andrew Jones <ajones@digitalbazaar.com>
2020-02-10 16:52:59 -0500

    Add setup steps for getInvokedCapability.
      - skip all tests that have not been implemented.
      - add basic headers for signing.

--- tests/10-verify.spec.js
+++ tests/10-verify.spec.js
@@ -1,5 +1,5 @@
 import {verifyCapabilityInvocation} from '..';
-import {suites} from 'jsonld-signatures';
+import {suites, SECURITY_CONTEXT_V2_URL} from 'jsonld-signatures';
 import {Ed25519KeyPair, RSAKeyPair} from 'crypto-ld';
 import uuid from 'uuid-random';
 
@@ -12,12 +12,16 @@ const Ed25519 = {
 };
 
 const Rsa = {
-  type: 'Rsa',
+  type: 'RSA',
   Suite: RsaSignature2018,
   LDKeyPair: RSAKeyPair
 };
 
 const controller = 'did:test:controller';
+const delegator = 'did:test:delegator';
+const invoker = 'did:test:invoker';
+const url = 'https://test.org/zcaps/foo';
+const expectedHost = 'test';
 
 const setup = async ({Suite, LDKeyPair}) => {
   const keyId = `did:key:${uuid()}`;
@@ -35,84 +39,119 @@ const setup = async ({Suite, LDKeyPair}) => {
     }
     throw new Error(`documentLoader unable to resolve ${url}`);
   };
-  return {keyId, keyPair, suite, documentLoader};
+  const getInvokedCapability = ({id, expectedTarget}) => {
+    return {
+      '@context': SECURITY_CONTEXT_V2_URL,
+      id: url,
+      invocationTarget: url,
+      controller,
+      delegator,
+      invoker
+    };
+  };
+  const created = Date.now() - 1000;
+  const expires = Date.now() + 10000;
+  const headers = {
+    url,
+    host: expectedHost,
+    date: created,
+    'capability-invocation': url,
+    authorization: `Signature keyId="${keyId}",created="${created}",` +
+      `expires="${expires}",requestTarget="${url}",signature="foo"`
+  };
+  return {keyId, keyPair, suite, documentLoader, getInvokedCapability, headers};
 };
 
 describe('verifyCapabilityInvocation', function() {
   [Ed25519, Rsa].forEach(function(suiteType) {
 
-    let suite, documentLoader, keyId = null;
+    describe(suiteType.type, function() {
+      let suite, documentLoader, keyId, getInvokedCapability, headers = null;
 
-    before(async function() {
-      ({suite, documentLoader, keyId} = await setup(suiteType));
-    });
+      beforeEach(async function() {
+        ({
+          suite,
+          documentLoader,
+          keyId,
+          headers,
+          getInvokedCapability
+        } = await setup(suiteType));
+      });
 
-    describe(suite.type, function() {
+      it('should verify a valid request', async function() {
+        console.log('headers', headers);
+        const result = await verifyCapabilityInvocation({
+          url,
+          suite,
+          headers,
+          getInvokedCapability,
+          documentLoader,
+          keyId
+        });
+        console.log('result', result);
+      });
 
-      it('should verify a valid requiest', async function() {
+      it.skip('should add headers if content-type is in headers', async function() {
 
       });
 
-      it('should add headers if content-type is in headers', async function() {
+      it.skip('should verify with additionalHeaders', async function() {
 
       });
 
-      it('should verify with additionalHeaders', async function() {
+      it.skip('should THROW if no getInvokedCapability', async function() {
 
       });
 
-      it('should THROW if no getInvokedCapability', async function() {
+      it.skip('should THROW if no documentLoader', async function() {
 
       });
 
-      it('should THROW if no documentLoader', async function() {
+      it.skip('should THROW if there are no headers', async function() {
 
       });
 
-      it('should THROW if verificationMethod type is not supported',
+      it.skip('should THROW if verificationMethod type is not supported',
         async function() {
 
         });
 
-      it('should NOT verify if there is no url', async function() {
+      it.skip('should NOT verify if there is no url', async function() {
 
       });
 
-      it('should NOT verify if there are no headers', async function() {
+
+      it.skip('should NOT verify if host is not in expectedHost', async function() {
 
       });
 
-      it('should NOT verify if host is not in expectedHost', async function() {
-
-      });
-
-      it('should NOT verify if keyId can not be dereferenced by the ' +
+      it.skip('should NOT verify if keyId can not be dereferenced by the ' +
         'documentLoader', async function() {
 
       });
 
-      it('should NOT verify if headers is missing (key-id)', async function() {
+      it.skip('should NOT verify if headers is missing (key-id)', async function() {
 
       });
 
-      it('should NOT verify if headers is missing (created)', async function() {
+      it.skip('should NOT verify if headers is missing (created)', async function() {
 
       });
 
-      it('should NOT verify if headers is missing (expires)', async function() {
+      it.skip('should NOT verify if headers is missing (expires)', async function() {
 
       });
 
-      it('should NOT verify if headers is missing (request-target)',
+      it.skip('should NOT verify if headers is missing (request-target)',
         async function() {
 
         });
 
-      it('should NOT verify if headers is missing host', async function() {
+      it.skip('should NOT verify if headers is missing host', async function() {
 
       });
 
-      it('should NOT verify if headers is missing capability-invocation',
+      it.skip('should NOT verify if headers is missing capability-invocation',
         async function() {
 
         });
