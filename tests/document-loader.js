2b19d3aa6ac8e813bf8179b0198e0517312ff78f
Dave Longley <dlongley@digitalbazaar.com>
2025-10-08 16:06:34 -0400

    Remove unused contexts.

--- tests/document-loader.js
+++ tests/document-loader.js
@@ -1,18 +1,12 @@
 /*!
- * Copyright (c) 2020-2022 Digital Bazaar, Inc. All rights reserved.
+ * Copyright (c) 2020-2025 Digital Bazaar, Inc. All rights reserved.
  */
-import aesContext from 'aes-key-wrapping-2019-context';
-import hmacContext from 'sha256-hmac-key-2019-context';
 import secCtx from '@digitalbazaar/security-context';
 import {securityLoader} from '@digitalbazaar/security-document-loader';
 import zcapCtx from 'zcap-context';
 
 const loader = securityLoader();
 loader.addStatic(zcapCtx.CONTEXT_URL, zcapCtx.CONTEXT);
-loader.addStatic(
-  aesContext.constants.CONTEXT_URL, aesContext.contexts);
-loader.addStatic(
-  hmacContext.constants.CONTEXT_URL, hmacContext.contexts);
 loader.addStatic(
   secCtx.SECURITY_CONTEXT_V2_URL,
   secCtx.contexts.get(secCtx.SECURITY_CONTEXT_V2_URL)
